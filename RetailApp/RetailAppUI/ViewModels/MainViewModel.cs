83782824fb0e8dac8ce3e1d9e99201b4b2d993ee
Rehcra001 <tkropf12345@gmail.com>
2023-09-19 13:07:55 +0200

    Added ability to open the customer view

--- RetailApp/RetailAppUI/ViewModels/MainViewModel.cs
+++ RetailApp/RetailAppUI/ViewModels/MainViewModel.cs
@@ -20,11 +20,23 @@ namespace RetailAppUI.ViewModels
 
 
         public RelayCommand NavigateToVendorViewCommand { get; set; }
+        public RelayCommand NavigateToCustomerViewModel { get; set; }
         public MainViewModel(INavigationService navigation)
         {
             Navigation = navigation;
 
             NavigateToVendorViewCommand = new RelayCommand(NavigateToVendorView, CanNavigateToVendorView);
+            NavigateToCustomerViewModel = new RelayCommand(NavigateToCustomerView, CanNavigateToCustomerView);
+        }
+
+        private bool CanNavigateToCustomerView(object obj)
+        {
+            return true;
+        }
+
+        private void NavigateToCustomerView(object obj)
+        {
+            Navigation.NavigateTo<CustomerViewModel>();
         }
 
         private bool CanNavigateToVendorView(object obj)
