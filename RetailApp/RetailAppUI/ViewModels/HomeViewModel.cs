027b19a735d02e4b84a60979b4ebaae0659c2ec1
Rechra001 <tkropf12345@gmail.com>
2023-09-19 11:21:47 +0200

    Home view added. Empty view that comes up when another view is closed

---
+++ RetailApp/RetailAppUI/ViewModels/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLibrary.VendorRepository;
+using ModelsLibrary;
+using RetailAppUI.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RetailAppUI.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        
+    }
+}
