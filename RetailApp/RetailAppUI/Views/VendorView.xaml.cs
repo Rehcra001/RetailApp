2cb7e4cb1f7adf4ecb2d1d1570735f899b2ac011
Rechra001 <tkropf12345@gmail.com>
2023-09-19 11:24:56 +0200

    Vendor view altered: Key trapping added for text boxes that required digits only; Phone number split into 3 textboxes

--- RetailApp/RetailAppUI/Views/VendorView.xaml.cs
+++ RetailApp/RetailAppUI/Views/VendorView.xaml.cs
@@ -24,5 +24,47 @@ namespace RetailAppUI.Views
         {
             InitializeComponent();
         }
+
+        private void TxtNumericOnly_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Limits text to digits only and a max length of 3 digits
+            if ((e.Key >= Key.D0 && e.Key <= Key.D9) || (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9))
+            {
+                //Allowed key presses
+                e.Handled = false;
+            }
+            else if (e.Key == Key.Back)
+            {
+                e.Handled = false;
+            }
+            else if (e.Key == Key.Tab)
+            {
+                e.Handled = false;
+            }
+            else if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+            else
+                    {
+                        e.Handled = true;
+                    }
+        }
+
+        private void TxtPhone_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox box = (TextBox)sender;
+            e.Handled = box.Text.Length > 2;
+        }
+
+        private void TxtPhoneSuffix_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox box = (TextBox)sender;
+            if (box.Text.Length > 3)
+            {
+                e.Handled = true;
+            }
+            
+        }
     }
 }
