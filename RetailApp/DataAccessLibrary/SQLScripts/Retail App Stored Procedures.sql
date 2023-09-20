107513e3038964ed514d8fb7b392fccc17d585d9
Rechra001 <tkropf12345@gmail.com>
2023-09-20 06:10:34 +0200

    Stored procedure script for customers added

--- RetailApp/DataAccessLibrary/SQLScripts/Retail App Stored Procedures.sql
+++ RetailApp/DataAccessLibrary/SQLScripts/Retail App Stored Procedures.sql
@@ -1,7 +1,7 @@
 USE RetailAppDB;
 GO
 
---**********Vendor--**********
+--**********Vendors**********
 CREATE PROCEDURE dbo.usp_InsertVendor
 (
 	@VatRegistrationNumber NVARCHAR(10),
@@ -165,6 +165,178 @@ BEGIN
 			   City, Province, PostalCode, EMailAddress, PhoneNumber
 		FROM dbo.Vendors
 		WHERE VendorID = @VendorID;
+	END TRY
+
+	BEGIN CATCH
+		SELECT ERROR_MESSAGE() AS Message;
+	END CATCH;
+END;
+GO
+
+--**********Customers**********
+CREATE PROCEDURE dbo.usp_InsertCustomer
+(
+	@VatRegistrationNumber NVARCHAR(10),
+	@FirstName NVARCHAR(100),
+	@LastName NVARCHAR(100),
+	@CompanyName NVARCHAR(100),
+	@AddressLine1 NVARCHAR(255),
+	@AddressLine2 NVARCHAR(255),
+	@City NVARCHAR(50),
+	@Province NVARCHAR(50),
+	@PostalCode NVARCHAR(10),
+	@EMailAddress NVARCHAR(255),
+	@PhoneNumber NVARCHAR(10)
+)AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRAN
+			SET NOCOUNT ON;
+
+			INSERT INTO dbo.Customers
+			(
+				VatRegistrationNumber,
+				FirstName,
+				LastName,
+				CompanyName,
+				AddressLine1,
+				AddressLine2,
+				City,
+				Province,
+				PostalCode,
+				EMailAddress,
+				PhoneNumber
+			)
+			VALUES
+			(
+				@VatRegistrationNumber,
+				@FirstName,
+				@LastName,
+				@CompanyName,
+				@AddressLine1,
+				@AddressLine2,
+				@City,
+				@Province,
+				@PostalCode,
+				@EMailAddress,
+				@PhoneNumber
+			);
+
+			SELECT SCOPE_IDENTITY() AS ID;
+		COMMIT TRAN
+	END TRY
+
+	BEGIN CATCH
+		IF (@@TRANCOUNT > 0)
+		BEGIN
+			ROLLBACK TRAN;
+		END
+		SELECT ERROR_MESSAGE() AS Message;
+	END CATCH;
+END;
+GO
+
+CREATE PROCEDURE dbo.usp_UpdateCustomer
+(
+	@CustomerID INT,
+	@VatRegistrationNumber NVARCHAR(10),
+	@FirstName NVARCHAR(100),
+	@LastName NVARCHAR(100),
+	@CompanyName NVARCHAR(100),
+	@AddressLine1 NVARCHAR(255),
+	@AddressLine2 NVARCHAR(255),
+	@City NVARCHAR(50),
+	@Province NVARCHAR(50),
+	@PostalCode NVARCHAR(10),
+	@EMailAddress NVARCHAR(255),
+	@PhoneNumber NVARCHAR(10)
+)AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRAN
+			SET NOCOUNT ON;
+
+			UPDATE dbo.Customers
+			SET	VatRegistrationNumber = @VatRegistrationNumber,
+				FirstName = @FirstName,
+				LastName = @LastName,
+				CompanyName = @CompanyName,
+				AddressLine1 = @AddressLine1,
+				AddressLine2 = @AddressLine2,
+				City = @City,
+				Province = @Province,
+				PostalCode = @PostalCode,
+				EMailAddress = @EMailAddress,
+				PhoneNumber = @PhoneNumber
+			WHERE CustomerID	= @CustomerID;
+
+			SELECT 'No Error' AS Message;
+		COMMIT TRAN
+	END TRY
+
+	BEGIN CATCH
+		IF (@@TRANCOUNT > 0)
+		BEGIN
+			ROLLBACK TRAN;
+		END
+		SELECT ERROR_MESSAGE() AS Message;
+	END CATCH;
+END;
+GO
+
+CREATE PROCEDURE dbo.usp_DeleteCustomer
+(
+	@CustomerID INT
+)AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRAN
+			DELETE FROM dbo.Customers
+			WHERE CustomerID = @CustomerID;
+
+			SELECT 'No Error' AS Message;
+		COMMIT TRAN;
+	END TRY
+
+	BEGIN CATCH
+		IF (@@TRANCOUNT > 0)
+		BEGIN
+			ROLLBACK TRAN;
+		END;
+		SELECT ERROR_MESSAGE() AS Message;
+	END CATCH;
+END;
+GO
+
+CREATE PROCEDURE dbo.usp_GetAllCustomers AS
+BEGIN
+	BEGIN TRY
+		SET NOCOUNT ON;
+
+		SELECT CustomerID, VatRegistrationNumber, FirstName, LastName, CompanyName, AddressLine1, AddressLine2,
+			   City, Province, PostalCode, EMailAddress, PhoneNumber
+		FROM dbo.Customers
+		ORDER BY CompanyName;
+	END TRY
+
+	BEGIN CATCH
+		SELECT ERROR_MESSAGE() AS Message;
+	END CATCH;
+END;
+GO
+
+CREATE PROCEDURE dbo.usp_GetCustomerByID
+(
+	@CustomerID INT
+)AS
+BEGIN
+	BEGIN TRY
+		SET NOCOUNT ON;
+
+		SELECT CustomerID, VatRegistrationNumber, FirstName, LastName, CompanyName, AddressLine1, AddressLine2,
+			   City, Province, PostalCode, EMailAddress, PhoneNumber
+		FROM dbo.Customers
+		WHERE CustomerID = @CustomerID;
 	END TRY
 
 	BEGIN CATCH
