78f02f99406c4f39b46db641628c714d924663ad
Rechra001 <tkropf12345@gmail.com>
2023-09-21 09:38:37 +0200

    Singleton service added in order to share data between view models

---
+++ RetailApp/RetailAppUI/Services/SharedDataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailAppUI.Services
+{
+    public class SharedDataService : ISharedDataService
+    {
+        private object _sharedData;
+
+        /// <summary>
+        /// A singleton property that can be used to share data between viewModels
+        /// </summary>
+        public object SharedData { get => _sharedData; set => _sharedData = value; }
+    }
+}
