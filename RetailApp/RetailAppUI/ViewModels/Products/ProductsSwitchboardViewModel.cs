9a5769d36adb70d5e9a907d6793963ddc10c6c42
Rechra001 <tkropf12345@gmail.com>
2023-09-24 11:33:07 +0200

    Added grouping to the data grid view

--- RetailApp/RetailAppUI/ViewModels/Products/ProductsSwitchboardViewModel.cs
+++ RetailApp/RetailAppUI/ViewModels/Products/ProductsSwitchboardViewModel.cs
@@ -5,17 +5,22 @@ using RetailAppUI.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
 
 namespace RetailAppUI.ViewModels.Products
 {
     public class ProductsSwitchboardViewModel : BaseViewModel
     {
-		private ProductsManager _productManager;
+		private readonly ProductsManager _productManager;
 
-		private ICurrentViewService	_currentView;
+		public ICollectionView ProductsCollectionView { get; set; }
+
+        private ICurrentViewService	_currentView;
 		public ICurrentViewService CurrentView
 		{
 			get { return _currentView; }
@@ -39,17 +44,24 @@ namespace RetailAppUI.ViewModels.Products
         private IConnectionStringService _connectionString;
         public IConnectionStringService ConnectionString { get => _connectionString; set => _connectionString = value; }
 
-        private ObservableCollection<ProductModel> _products;        
-
+        private ObservableCollection<ProductModel> _products;       
         public ObservableCollection<ProductModel> Products
 		{
 			get { return _products; }
 			set { _products = value; OnPropertyChanged(); }
 		}
 
+		private ProductModel _product;
+
+		public ProductModel Product
+		{
+			get { return _product; }
+			set { _product = value; OnPropertyChanged(); }
+		}
 
 
 		public RelayCommand CloseViewCommand { get; set; }
+		public RelayCommand NavigateToAddNewProductViewCommand { get; set; }
 
         public ProductsSwitchboardViewModel(INavigationService navigation, ICurrentViewService currentView, ISharedDataService sharedData, IConnectionStringService connectionString)
         {
@@ -58,11 +70,50 @@ namespace RetailAppUI.ViewModels.Products
 			SharedData = sharedData;
 			ConnectionString = connectionString;
 
-			_productManager = new ProductsManager(ConnectionString.GetConnectionString());
-			Products = new ObservableCollection<ProductModel>(_productManager.GetAll());
-
+			//Fetch a list of products
+            _productManager = new ProductsManager(ConnectionString.GetConnectionString());
+            GetProductsList();
 
 			CloseViewCommand = new RelayCommand(CloseView, CanCloseView);
+			NavigateToAddNewProductViewCommand = new RelayCommand(NavigateToAddNewProductView, CanNavigateToAddNewProductView);
+        }
+
+        #region ProductsMethods
+        private void GetProductsList()
+		{
+			try
+			{
+                Products = new ObservableCollection<ProductModel>(_productManager.GetAll());
+                SetProductCollectionView();
+            }
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Error Retrieving Products", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
+        }
+
+		private void SetProductCollectionView()
+		{
+			ProductsCollectionView = CollectionViewSource.GetDefaultView(Products);
+			SetProductGrouping();
+		}
+
+		private void SetProductGrouping()
+		{
+			ProductsCollectionView.GroupDescriptions.Add(new PropertyGroupDescription("Vendor.CompanyName"));
+		}
+        #endregion ProductsMethods
+
+        private bool CanNavigateToAddNewProductView(object obj)
+        {
+			return true;
+        }
+
+        private void NavigateToAddNewProductView(object obj)
+        {
+
+			Navigation.NavigateTo<AddNewProductViewModel>();
         }
 
         private bool CanCloseView(object obj)
