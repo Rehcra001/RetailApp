a6bb5e87b91414b687c24adb535f6ea4542ca137
Rechra001 <tkropf12345@gmail.com>
2023-09-24 11:44:01 +0200

    Category Model added

---
+++ RetailApp/ModelLibrary/CategoryModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsLibrary
+{
+    public class CategoryModel
+    {
+        /// <summary>
+        /// Holds the unique category ID
+        /// </summary>
+        public int CategoryID { get; set; }
+
+        /// <summary>
+        /// Holds the category Name
+        /// </summary>
+        public string? CategoryName { get; set; }
+
+        public string? ValidationMessage { get; set; }
+
+
+        /// <summary>
+        /// Validates the data entered. 
+        /// ValidationMessage property is populated with the errors list if any errors encountered.
+        /// </summary>
+        /// <returns>
+        /// True if no errors encountered - false otherwise. If false see ValidationMessage property
+        /// </returns>
+        public bool Validate()
+        {
+            bool isValid = true;
+            ValidationMessage = "";
+
+            if (string.IsNullOrWhiteSpace(CategoryName))
+            {
+                ValidationMessage += "Category Name is required.\r\n";
+                isValid = false;
+            }
+            else if (CategoryName.Length > 50)
+            {
+                ValidationMessage += "Category Name cannot have more than 50 characters.\r\n";
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+}
