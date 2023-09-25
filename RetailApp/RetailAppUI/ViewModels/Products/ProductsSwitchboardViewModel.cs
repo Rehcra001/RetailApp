05e4f05fff9ff1a3bc29cbbb92bcb755817d90c7
Rechra001 <tkropf12345@gmail.com>
2023-09-25 07:34:54 +0200

    Added a filter to the products list

--- RetailApp/RetailAppUI/ViewModels/Products/ProductsSwitchboardViewModel.cs
+++ RetailApp/RetailAppUI/ViewModels/Products/ProductsSwitchboardViewModel.cs
@@ -15,7 +15,8 @@ namespace RetailAppUI.ViewModels.Products
 		private readonly ProductsManager _productsManager;
 		private string _groupByState;
 
-		public ICollectionView ProductsCollectionView { get; set; }
+        #region Collection View Property
+        public ICollectionView ProductsCollectionView { get; set; }
 
         private ICurrentViewService	_currentView;
 		public ICurrentViewService CurrentView
@@ -23,8 +24,10 @@ namespace RetailAppUI.ViewModels.Products
 			get { return _currentView; }
 			set { _currentView = value; OnPropertyChanged(); }
 		}
+        #endregion Collection View Property
 
-		private INavigationService _navigation;
+        #region Service Properties
+        private INavigationService _navigation;
 		public INavigationService Navigation
 		{
 			get { return _navigation; }
@@ -36,11 +39,13 @@ namespace RetailAppUI.ViewModels.Products
 		{
 			get { return _sharedData; }
 			set { _sharedData = value; }
-		}
+		}        
 
         private IConnectionStringService _connectionString;
         public IConnectionStringService ConnectionString { get => _connectionString; set => _connectionString = value; }
+        #endregion Service Properties
 
+        #region Product Properties
         private ObservableCollection<ProductModel> _products;       
         public ObservableCollection<ProductModel> Products
 		{
@@ -49,20 +54,33 @@ namespace RetailAppUI.ViewModels.Products
 		}
 
 		private ProductModel _product;
-
 		public ProductModel Product
 		{
 			get { return _product; }
 			set { _product = value; OnPropertyChanged(); }
 		}
+        #endregion Product Properties
 
+        #region Product Filter Properties
+        private string _productNameFilter = string.Empty;
+		public string ProductNameFilter
+		{
+			get { return _productNameFilter; }
+			set { _productNameFilter = value; OnPropertyChanged(); ProductsCollectionView.Refresh(); }
+		}
+        #endregion Product Filter Properties
 
-		public RelayCommand CloseViewCommand { get; set; }
+        #region Relay Command Properties
+        //commands
+        public RelayCommand CloseViewCommand { get; set; }
 		public RelayCommand NavigateToAddNewProductViewCommand { get; set; }
+        public RelayCommand NavigateToProductViewCommand { get; set; }
         public RelayCommand GroupByCategoryCommand { get; set; }
         public RelayCommand GroupByVendorCommand { get; set; }
         public RelayCommand ClearGroupByCommand { get; set; }
+        #endregion Relay Command Properties
 
+        #region Constructor
         public ProductsSwitchboardViewModel(INavigationService navigation, ICurrentViewService currentView, ISharedDataService sharedData, IConnectionStringService connectionString)
         {
 			Navigation = navigation;
@@ -76,12 +94,15 @@ namespace RetailAppUI.ViewModels.Products
 
 			CloseViewCommand = new RelayCommand(CloseView, CanCloseView);
 			NavigateToAddNewProductViewCommand = new RelayCommand(NavigateToAddNewProductView, CanNavigateToAddNewProductView);
+			NavigateToProductViewCommand = new RelayCommand(NavigateToProductView, CanNavigateToProductView);
 			GroupByCategoryCommand = new RelayCommand(GroupByCategory, CanGroupByCategory);
 			GroupByVendorCommand = new RelayCommand(GroupByVendor, CanGroupByVendor);
 			ClearGroupByCommand = new RelayCommand(ClearGroupBy, CanClearGroupBy);
 
 			SetGroupByState("Clear");
         }
+        #endregion Constructor
+
 
         #region GroupBy Methods
         private bool CanGroupByVendor(object obj)
@@ -145,7 +166,7 @@ namespace RetailAppUI.ViewModels.Products
 		}
         #endregion GroupBy Methods
 
-        #region ProductsMethods
+        #region Products List Methods
         private void GetProductsList()
 		{
 			try
@@ -163,10 +184,24 @@ namespace RetailAppUI.ViewModels.Products
 		private void SetProductCollectionView()
 		{
 			ProductsCollectionView = CollectionViewSource.GetDefaultView(Products);
+			//Add a filter
+			ProductsCollectionView.Filter = FilterProductName;
 		}
-		#endregion ProductsMethods
+        #endregion Products List Methods
 
-		private bool CanNavigateToAddNewProductView(object obj)
+        #region Filter Methods
+        private bool FilterProductName(object obj)
+        {
+            if (obj is ProductModel product)
+			{
+				return product.ProductName.Contains(ProductNameFilter, StringComparison.InvariantCultureIgnoreCase);
+			}
+			return false;
+        }
+        #endregion Filter Methods
+
+        #region Navigate To Views
+        private bool CanNavigateToAddNewProductView(object obj)
         {
 			return true;
         }
@@ -177,6 +212,18 @@ namespace RetailAppUI.ViewModels.Products
 			Navigation.NavigateTo<AddNewProductViewModel>();
         }
 
+        private bool CanNavigateToProductView(object obj)
+        {
+			return Product != null;
+        }
+
+        private void NavigateToProductView(object obj)
+        {
+			Navigation.NavigateTo<ProductViewModel>();
+        }
+        #endregion Navigate To Views
+
+        #region Close View
         private bool CanCloseView(object obj)
         {
             return true;
@@ -187,5 +234,6 @@ namespace RetailAppUI.ViewModels.Products
             CurrentView.CurrentView = "HomeView";
             Navigation.NavigateTo<HomeViewModel>();
         }
+        #endregion Close View
     }
 }
