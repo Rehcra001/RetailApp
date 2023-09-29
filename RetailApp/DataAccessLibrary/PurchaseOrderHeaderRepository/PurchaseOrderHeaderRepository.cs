3eff995f1d1c7d3f5cc1c35f8821062ffa6276e8
Rehcra001 <tkropf12345@gmail.com>
2023-09-29 13:20:42 +0200

    Added purchase order header repository

---
+++ RetailApp/DataAccessLibrary/PurchaseOrderHeaderRepository/PurchaseOrderHeaderRepository.cs
@@ -0,0 +1,201 @@
+﻿using ModelsLibrary;
+using ModelsLibrary.RepositoryInterfaces;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccessLibrary.PurchaseOrderHeaderRepository
+{
+    public class PurchaseOrderHeaderRepository : IPurchaseOrderHeaderRepository
+    {
+        private string _connectionString;
+
+        public PurchaseOrderHeaderRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public (IEnumerable<PurchaseOrderHeaderModel>, string) GetALL()
+        {
+            List<PurchaseOrderHeaderModel> purchaseOrderHeaders = new List<PurchaseOrderHeaderModel>();
+            string? errorMessage = null;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "dbo.usp_GetAllPurchaseOrderHeaders";
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        //Check for errors
+                        //Expecting more than one column on no error
+                        if (reader.FieldCount > 1)
+                        {
+                            //no error
+                            //check for data
+                            if (reader.HasRows)
+                            {
+                                //Contains data
+                                while (reader.Read())
+                                {
+                                    PurchaseOrderHeaderModel purchaseOrderHeader = new PurchaseOrderHeaderModel();
+                                    purchaseOrderHeader.PurchaseOrderID = Convert.ToInt64(reader["PurchaseOrderID"]);
+                                    purchaseOrderHeader.VendorID = Convert.ToInt32(reader["VendorID"]);
+                                    purchaseOrderHeader.VendorReference = reader["VendorReference"].ToString();
+                                    purchaseOrderHeader.OrderDate = Convert.ToDateTime(reader["OrderDate"]);
+                                    purchaseOrderHeader.OrderAmount = Convert.ToDecimal(reader["OrderAmount"]);
+                                    purchaseOrderHeader.VATPercentage = Convert.ToDecimal(reader["VATPercentage"]);
+                                    purchaseOrderHeader.VATAmount = Convert.ToDecimal(reader["VATAmount"]);
+                                    purchaseOrderHeader.TotalAmount = Convert.ToDecimal(reader["TotalAmount"]);
+                                    purchaseOrderHeader.RequiredDate = (DateOnly)reader["RequiredDate"];
+                                    purchaseOrderHeader.OrderStatusID = Convert.ToInt32(reader["OrderStatusID"]);
+
+                                    purchaseOrderHeaders.Add(purchaseOrderHeader);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            //error raised
+                            reader.Read();
+                            errorMessage = reader["Message"].ToString();
+                        }
+                    }
+
+                }
+            }
+
+            return (purchaseOrderHeaders, errorMessage); //Error message will be null if no error raised by database
+        }
+
+        public (PurchaseOrderHeaderModel, string) GetByID(long id)
+        {
+            PurchaseOrderHeaderModel purchaseOrderHeader = new PurchaseOrderHeaderModel();
+            string? errorMessage = null;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "dbo.usp_GetPurchaseOrderHeaderByID";
+                    command.Parameters.Add("@PurchaseOrderID", SqlDbType.BigInt).Value = id;
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        //Check for errors
+                        //Expecting more than one column on no error
+                        if (reader.FieldCount > 1)
+                        {
+                            //no error
+                            //check for data
+                            if (reader.HasRows)
+                            {
+                                //Contains data
+                                reader.Read();
+                                purchaseOrderHeader.PurchaseOrderID = Convert.ToInt64(reader["PurchaseOrderID"]);
+                                purchaseOrderHeader.VendorID = Convert.ToInt32(reader["VendorID"]);
+                                purchaseOrderHeader.VendorReference = reader["VendorReference"].ToString();
+                                purchaseOrderHeader.OrderDate = Convert.ToDateTime(reader["OrderDate"]);
+                                purchaseOrderHeader.OrderAmount = Convert.ToDecimal(reader["OrderAmount"]);
+                                purchaseOrderHeader.VATPercentage = Convert.ToDecimal(reader["VATPercentage"]);
+                                purchaseOrderHeader.VATAmount = Convert.ToDecimal(reader["VATAmount"]);
+                                purchaseOrderHeader.TotalAmount = Convert.ToDecimal(reader["TotalAmount"]);
+                                purchaseOrderHeader.RequiredDate = (DateOnly)reader["RequiredDate"];
+                                purchaseOrderHeader.OrderStatusID = Convert.ToInt32(reader["OrderStatusID"]);
+                            }
+                        }
+                        else
+                        {
+                            //error raised
+                            reader.Read();
+                            errorMessage = reader["Message"].ToString();
+                        }
+                    }
+                }
+            }
+
+            return (purchaseOrderHeader, errorMessage); //Error Message will be null if no error raised by database
+        }
+
+        public (PurchaseOrderHeaderModel, string) Insert(PurchaseOrderHeaderModel purchaseOrderHeader)
+        {
+            string? errorMessage = null;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "dbo.usp_InsertPurchaseOrderHeader";
+                    command.Parameters.Add("@VendorID", SqlDbType.Int).Value = purchaseOrderHeader.VendorID;
+                    command.Parameters.Add("@VendorReference", SqlDbType.NVarChar).Value = purchaseOrderHeader.VendorReference;
+                    command.Parameters.Add("@OrderDate", SqlDbType.DateTime).Value = purchaseOrderHeader.OrderDate;
+                    command.Parameters.Add("@OrderAmount", SqlDbType.Money).Value = purchaseOrderHeader.OrderAmount;
+                    command.Parameters.Add("@VATPercentage", SqlDbType.Decimal).Value = purchaseOrderHeader.VATPercentage;
+                    command.Parameters.Add("@VATAmount", SqlDbType.Money).Value = purchaseOrderHeader.VATAmount;
+                    command.Parameters.Add("@TotalAmount", SqlDbType.Money).Value = purchaseOrderHeader.TotalAmount;
+                    command.Parameters.Add("@RequiredDate", SqlDbType.Date).Value = purchaseOrderHeader.RequiredDate;
+                    command.Parameters.Add("@OrderStatusID", SqlDbType.Int).Value = purchaseOrderHeader.OrderStatusID;
+                    connection.Open();
+
+                    string returnedMessage = command.ExecuteScalar().ToString()!;
+                    //Check for errors
+                    if (long.TryParse(returnedMessage, out _))
+                    {
+                        //no error
+                        purchaseOrderHeader.PurchaseOrderID = long.Parse(returnedMessage);
+                    }
+                    else
+                    {
+                        //error raised
+                        errorMessage = returnedMessage;
+                    }
+                }
+            }
+            return (purchaseOrderHeader, errorMessage);//Error message will be null if no error raised by database
+        }
+
+        public string Update(PurchaseOrderHeaderModel purchaseOrderHeader)
+        {
+            string? errorMessage = null;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "dbo.usp_UpdatePurchaseOrderHeader";
+                    command.Parameters.Add("@PurchaseOrderID", SqlDbType.BigInt).Value = purchaseOrderHeader.PurchaseOrderID;
+                    command.Parameters.Add("@VendorID", SqlDbType.Int).Value = purchaseOrderHeader.VendorID;
+                    command.Parameters.Add("@VendorReference", SqlDbType.NVarChar).Value = purchaseOrderHeader.VendorReference;
+                    command.Parameters.Add("@OrderDate", SqlDbType.DateTime).Value = purchaseOrderHeader.OrderDate;
+                    command.Parameters.Add("@OrderAmount", SqlDbType.Money).Value = purchaseOrderHeader.OrderAmount;
+                    command.Parameters.Add("@VATPercentage", SqlDbType.Decimal).Value = purchaseOrderHeader.VATPercentage;
+                    command.Parameters.Add("@VATAmount", SqlDbType.Money).Value = purchaseOrderHeader.VATAmount;
+                    command.Parameters.Add("@TotalAmount", SqlDbType.Money).Value = purchaseOrderHeader.TotalAmount;
+                    command.Parameters.Add("@RequiredDate", SqlDbType.Date).Value = purchaseOrderHeader.RequiredDate;
+                    command.Parameters.Add("@OrderStatusID", SqlDbType.Int).Value = purchaseOrderHeader.OrderStatusID;
+                    connection.Open();
+
+                    string returnedMessage = command.ExecuteScalar().ToString()!;
+
+                    if (!returnedMessage.Equals("No Error"))
+                    {
+                        //Error raised
+                        errorMessage = returnedMessage;
+                    }
+                }
+            }
+
+            return errorMessage; //Will be null if no error raised by the database
+        }
+    }
+}
---
+++ RetailApp/ModelLibrary/RepositoryInterfaces/IPurchaseOrderHeaderRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ModelsLibrary.RepositoryInterfaces
+{
+    public interface IPurchaseOrderHeaderRepository
+    {
+        (PurchaseOrderHeaderModel, string) Insert(PurchaseOrderHeaderModel purchaseOrderHeader);
+        string Update(PurchaseOrderHeaderModel purchaseOrderHeader);
+        (IEnumerable<PurchaseOrderHeaderModel>, string) GetALL();
+        (PurchaseOrderHeaderModel, string) GetByID(long id);
+    }
+}
