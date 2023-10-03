ecb391da0a8c0552b954c925f1425b0dc8667243
Rechra001 <tkropf12345@gmail.com>
2023-10-03 10:36:49 +0200

    Added GetByOrderStatus method

--- RetailApp/ModelLibrary/RepositoryInterfaces/IPurchaseOrderHeaderRepository.cs
+++ RetailApp/ModelLibrary/RepositoryInterfaces/IPurchaseOrderHeaderRepository.cs
@@ -5,6 +5,7 @@
         (PurchaseOrderHeaderModel, string) Insert(PurchaseOrderHeaderModel purchaseOrderHeader);
         string Update(PurchaseOrderHeaderModel purchaseOrderHeader);
         (IEnumerable<PurchaseOrderHeaderModel>, string) GetALL();
+        (IEnumerable<PurchaseOrderHeaderModel>, string) GetByOrderStatus(string orderStatus);
         (PurchaseOrderHeaderModel, string) GetByID(long id);
     }
 }
